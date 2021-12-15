d3784ba304fee54fe1762c04d7ca953f6f88834e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2021-12-15 23:27:33 +0530

    day 35 correction

--- day_35.js
+++ day_35.js
@@ -6,11 +6,14 @@
 
 function nthlargest(arr, highest) {
 	// write your code here
-    arr.sort();
-	return arr[(arr.length-(highest-1))];
+    arr.sort((a, b)=>(a-b));
+    console.log(arr);
+	return arr[(arr.length-highest)];
 }
 
 const arr = [43, 56, 23, 89, 88, 90, 99, 652];
 const highest = 4;
 
 console.log(nthlargest(arr, highest));
+console.log(nthlargest([ 10, 100, 1000, 10000], 2));
+console.log(nthlargest([ 1, 4, 6, 10], 2))
